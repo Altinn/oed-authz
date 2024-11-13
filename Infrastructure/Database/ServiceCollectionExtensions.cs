5860a592d19daf026a31c4fdb03c1468113e975d
Lars Olav Kjølstad <lars.olav.kjolstad@digdir.no>
2024-11-13 12:12:05 +0100

    Efcore (#15)
    Fjernet yuniql og erstattet med ef core

---
+++ Infrastructure/Database/ServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using oed_authz.Settings;
+
+namespace oed_authz.Infrastructure.Database;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddOedAuthzDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContextPool<OedAuthzDbContext>((IServiceProvider provider, DbContextOptionsBuilder optionsBuilder) =>
+        {
+            var secrets = provider.GetRequiredService<IOptions<Secrets>>();
+            optionsBuilder.UseNpgsql(secrets.Value.PostgreSqlUserConnectionString);
+        });
+
+        return services;
+    }
+}
+
+public static class WebAppExtensions
+{
+    /// <summary>
+    /// Extension method for running database migrations using the admin connectionstring
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    public static async Task MigrateOedAuthzDatabase(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var loggerFactory = scope.ServiceProvider.GetRequiredService<ILoggerFactory>();
+        var secrets = scope.ServiceProvider.GetRequiredService<IOptions<Secrets>>();
+
+        var optionsBuilder = new DbContextOptionsBuilder<OedAuthzDbContext>()
+            .UseNpgsql(secrets.Value.PostgreSqlAdminConnectionString)
+            .UseLoggerFactory(loggerFactory);
+
+        using var dbContext = new OedAuthzDbContext(optionsBuilder.Options);
+        await dbContext.Database.MigrateAsync();
+    }
+}
+
---
+++ Interfaces/IRoleAssignmentsRepository.cs
@@ -0,0 +1,11 @@
+﻿using oed_authz.Models;
+
+namespace oed_authz.Interfaces;
+
+public interface IRoleAssignmentsRepository
+{
+    public Task<List<RoleAssignment>> GetRoleAssignmentsForEstate(string estateSsn, string? filterRecipentSsn = null, string? filterRoleCode = null);
+    public Task<List<RoleAssignment>> GetRoleAssignmentsForPerson(string recipientSsn, string? filterEstateSsn = null, string? filterRoleCode = null);
+    public Task AddRoleAssignment(RoleAssignment roleAssignment);
+    public Task RemoveRoleAssignment(RoleAssignment roleAssignment);
+}
